941ce1cbce1a9c1ff250c9ec9e2f501024588663
Joseph Martell <contact@josephmartell.com>
2023-05-17 14:15:01 +0200

    implement initial position

---
+++ src/MarsRover.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace GQ\Kata;
+
+class MarsRover
+{
+    public function execute(string $commands): string
+    {
+        return '0:0:N';
+    }
+}
---
+++ tests/MarsRoverTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace GQ\Kata\Tests;
+
+use GQ\Kata\MarsRover;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class MarsRoverTest extends TestCase
+{
+    #[Test]
+    public function should_not_move_when_executing_nothing(): void
+    {
+        $marsRover = new MarsRover();
+        $position = $marsRover->execute('');
+
+        $this->assertEquals('0:0:N', $position);
+    }
+}
